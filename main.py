33575e7851fa2a4cf5eef43638437e65dc13699a
Alex Besedin <alebesedin@yandex.ru>
2023-08-18 12:21:03 +0300

    Добавлен файл message_handler.py, исправления в файле main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import os
 import logging
-import requests
 import sqlite3
 import telebot
-from bs4 import BeautifulSoup
-from utils import check, get_connection, get_status
-from constants import (TELEGRAM_TOKEN, FORMAT, 
-                       MUON_TEXT, ABOUT_TEXT, 
-                       HELP_TEXT, LINKS_NODE_ID,
-                       TIMEOUT_ERROR, REQUEST_ERROR)
+from constants import TELEGRAM_TOKEN, FORMAT
+from message_handlers import (
+    handle_start, 
+    handle_help,
+    handle_about,
+    handle_muon,
+    handle_status,
+    handle_text_message)
 
 
 logging.basicConfig(
@@ -33,160 +33,38 @@ with sqlite3.connect('users.db') as conn:
 
 @bot.message_handler(commands=['start'])
 def start_command_handler(message):
-    """Обработчик команды /start"""
-    user_id = message.from_user.id
-    try:
-        with get_connection() as conn:
-            cursor = conn.cursor()
-            # Проверка, существует ли пользователь уже в базе данных
-            cursor.execute("SELECT * FROM users WHERE user_id=?", (user_id,))
-            row = cursor.fetchone()
-            if row is None:
-            # Если пользователь новый, добавляем его в базу данных
-                cursor.execute("INSERT INTO users (user_id) VALUES (?)", (user_id,))
-                conn.commit()
-            username = message.from_user.username
-            message_text = (
-                f"Hello, {username}!\n\n"
-                "*Enter the IP-address of your Muon Node:*\n"
-                "_Example:_"
-                )
-            bot.send_message(chat_id=message.chat.id,
-                         text=message_text,
-                         parse_mode='Markdown'
-                         )
-            image_path = os.path.join('pic', 'picpic.jpg')
-            with open(image_path, 'rb') as image:
-                bot.send_photo(chat_id=message.chat.id, 
-                           photo=image
-                           )
-    except Exception as e:
-        logger.error(f'An error occurred in the /start command handler: {str(e)}')
+    """Хендлер команды /start"""
+    handle_start(message, bot, logger)      
     
 
 @bot.message_handler(commands=['help'])
 def help_command_handler(message):
-    """Обработчик команды /help"""
-    bot.send_message(
-        chat_id=message.chat.id,
-        text=HELP_TEXT,
-        parse_mode='Markdown'
-        )
+    """Хендлер команды /help"""
+    handle_help(message)
 
 
 @bot.message_handler(commands=['about'])
 def about_command_handler(message):
-    """Обработчик команды /about"""
-    bot.send_message(
-        chat_id=message.chat.id,
-        text=ABOUT_TEXT,
-        parse_mode='Markdown'
-        )
+    """Хендлер команды /about"""
+    handle_about(message)
 
 
 @bot.message_handler(commands=['muon'])
 def muon_command_handler(message):
-    """Обработчик команды /muon"""
-    bot.send_message(
-        chat_id=message.chat.id,
-        text=MUON_TEXT,
-        parse_mode='Markdown'
-        )
+    """Хендлер команды /muon"""
+    handle_muon(message)
     
 
 @bot.message_handler(commands=['status'])
-def muon_command_handler(message):
-    """Обработчик команды /status"""
-    status_message = get_status()
-    bot.send_message(
-        chat_id=message.chat.id, 
-        text=status_message
-        )    
+def status_command_handler(message):
+    """Хендлер команды /status"""
+    handle_status(message, bot)
                
 
 @bot.message_handler(func=lambda message: True)
 def text_message_handler(message):
-    ip_address = message.text.strip()
-
-    if check(ip_address):
-        bot.send_message(
-            chat_id=message.chat.id,
-            text="The format of the IP address is incorrect. Please try again."
-        )
-        return
-
-    try:
-        response = requests.get(f"http://{ip_address}:8000/status", timeout=50)
-
-        if response.status_code == 200:
-            json_data = response.json()
-            node_info = json_data.get('network', {}).get('nodeInfo', {})
-            node_id = node_info.get('id', '')
-            node_active = node_info.get('active', '')
-            node_id_link = f"[{node_id}](https://explorer.muon.net/nodes/{node_id})"
-            uptime_info = json_data.get('node', {}).get('uptime', '')
-            networking_port = json_data.get('node', {}).get('networkingPort', '')
-            network_info = json_data.get('managerContract', {}).get('network', '')
-            staker_info = json_data.get('staker', '')
-            node_address_info = json_data.get('address', '')
-            perr_id_info = json_data.get('peerId', '')
-
-            try:
-                links_nodes = LINKS_NODE_ID + f'{node_id}'
-                response = requests.get(links_nodes, timeout=10)
-                soup = BeautifulSoup(response.text, 'lxml')
-                tag_div = soup.find(
-                    'div', class_='sc-12fa1d5b-0 fQGscc card rounded-4 p-3 position-relative overflow-hidden h-100'
-                    )
-                uptime_value = next(
-                    (tag_h6.text for tag_h6 in tag_div.find_all('h6') if '%' in tag_h6.text), 
-                    'Error parsing id data'
-                    )
-            except Exception as e:
-                logger.error(f'Error parsing id data: {str(e)}')
-                uptime_value = 'Error parsing id data'
-
-            node_address_short = f"{node_address_info[:6]}...{node_address_info[-6:]}"
-            staker_info_short = f"{staker_info[:6]}...{staker_info[-6:]}"
-            perr_id_info_short = f"{perr_id_info[:6]}...{perr_id_info[-6:]}"
-
-            message_text = (
-                f"👤 Node ID: {node_id_link}\n"
-                f"🟢 Active: {node_active}\n"
-                f"🌐 Uptime: {uptime_value}\n"
-                f"⚙️ Network: {network_info}\n"
-                f"📟 Uptime: `{uptime_info}`\n"
-                f"💰 Node: `{node_address_short}`\n"
-                f"📲 Staker: `{staker_info_short}`\n"
-                f"⛓ Peer id: `{perr_id_info_short}`\n"
-                f"🔋 NetworkingPort: `{networking_port}`\n"
-            )
-
-            bot.send_message(
-                chat_id=message.chat.id,
-                text=message_text,
-                parse_mode='Markdown'
-            )
-        else:
-            # Если запрос не выполнен успешно, отправляем сообщение об ошибке
-            bot.send_message(
-                chat_id=message.chat.id,
-                text="Error with IP address or node is disabled."
-            )
-    except requests.exceptions.Timeout as t:
-        logger.error(f'Request timed out: {str(t)}')
-        # Если запрос превысил время ожидания, отправляем сообщение об ошибке
-        bot.send_message(
-            chat_id=message.chat.id,
-            text=TIMEOUT_ERROR
-        )
-    except requests.exceptions.RequestException as r:
-        # Если возникла другая ошибка запроса, отправляем сообщение об ошибке
-        logger.error(f'Request error: {str(r)}')
-        bot.send_message(
-            chat_id=message.chat.id,
-            text=REQUEST_ERROR
-        )
+    """Хендлер тестовых сообщений"""
+    handle_text_message(message, bot)
 
             
 def main():
